16ef06fcbdb16e6f2e452877dfde908701b2da66
chocomountain1 <terminator32158@gmail.com>
2026-01-13 15:24:40 -0300

    Feature: added the variables.css and base.css styles

--- ecobarrios_ui/src/main.jsx
+++ ecobarrios_ui/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './styles/base.css' // importación de estilos base por si es necesario algún reset
+import './styles/variables.css' // importación de variables CSS, con estilo dark y "tipografia elegante"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
