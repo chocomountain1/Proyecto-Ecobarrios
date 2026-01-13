56e18cdf3721911747f0a6afd71ea312e1ad8eff
chocomountain1 <terminator32158@gmail.com>
2026-01-13 16:03:32 -0300

    Feature: add the fonts.css files and did some changes in order to made the button work

--- ecobarrios_ui/src/App.jsx
+++ ecobarrios_ui/src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import './styles/components/button.css'  // importación de estilos para botones personalizados
 function App() {
   return (
     <>
@@ -16,6 +16,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button className= "btn">
+          Hola
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
