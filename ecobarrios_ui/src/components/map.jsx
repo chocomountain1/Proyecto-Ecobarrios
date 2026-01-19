58ece67cb1ae56493b9c01c59dd38e11502f6b2a
chocomountain1 <terminator32158@gmail.com>
2026-01-19 16:10:19 -0300

    Feature: add a reactive response everytime that the user clicks on a pin, it display the contact info

--- ecobarrios_ui/src/components/map.jsx
+++ ecobarrios_ui/src/components/map.jsx
@@ -1,25 +1,36 @@
 import 'leaflet/dist/leaflet.css';
 import '../styles/components/elements/map.css';
+import '../styles/components/modal/modal-text.css';
 import "leaflet.awesome-markers/dist/leaflet.awesome-markers.css"; //markers personalizados
 import "@fortawesome/fontawesome-free/css/all.css"; //fuentes en los markers personalizados
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-import myMarker from './icons/markers';
+import MarkerComp from './markerComp';
+import Modal from './Modal';
+import React from 'react';
 import Sidebar from './sidebar';
-
-// Diccionario para desambiguar el estado de consolidación de los ecobarrios
-const consolidationStatus = {
-  "No participó en Sendero Ecobarrio" : "gray",
-  "Semilla" : "green",
-  "En Consolidación" : "orange",
-  "Referente" : "red"
-}
 
 export default function Map({ ecobarrios }) {
   console.log(ecobarrios)
   console.log(ecobarrios[0]);
+  const [selectedMarker, setSelectedMarker] = React.useState(null);
   return (
     <div className="map-container">
+        {selectedMarker && (
+        <Modal onClose={() => setSelectedMarker(null)}>
+          <div className = "modal-name">
+            <h1 className = "modal-h1-text">{selectedMarker.e.nombre}</h1>
+          </div>
+          <div className = "modal-section-title">
+            <h2 className = "modal-h2-text">Información de contacto</h2>
+            <div className = "modal-section-content">
+                <p className = "modal-p-text">📍<strong>Dirección:</strong> {selectedMarker.e.maps}</p>
+                <p className = "modal-p-text">📞<strong>Teléfono:</strong> {selectedMarker.e.telefono_contacto}</p>
+                <p className = "modal-p-text">📧<strong>Correo electrónico:</strong> {selectedMarker.e.correo_contacto}</p>
+            </div>
+          </div>
+        </Modal>
+        )}
       <div className="map-area">
         <MapContainer center={[-33.45, -70.66]} zoom={10} style={{ height: "100%", width: "100%" }}>
           <TileLayer
@@ -28,15 +39,7 @@ export default function Map({ ecobarrios }) {
           ></TileLayer>;
 
           {ecobarrios.map((e) => (
-            <MarkerClusterGroup>
-            <Marker icon= {myMarker(consolidationStatus[e.sendero_ecobarrio])} position={[e.lat, e.lon]} key={e.id}>
-              <Popup>
-                <div>
-                  <h3>{e.nombre}</h3>
-                </div>
-              </Popup>
-            </Marker>
-            </MarkerClusterGroup>
+             <MarkerComp e={e} onSelect={setSelectedMarker} />
           ))}
         </MapContainer>
         </div>
@@ -44,5 +47,6 @@ export default function Map({ ecobarrios }) {
         {<Sidebar />}
       </div>
     </div>
+    
   );
 }
---
+++ ecobarrios_ui/src/components/markercomp.jsx
@@ -0,0 +1,26 @@
+import { Marker, Popup} from "react-leaflet";
+import myMarker from './icons/markers';
+
+
+
+// Diccionario para desambiguar el estado de consolidación de los ecobarrios
+const consolidationStatus = {
+  "No participó en Sendero Ecobarrio" : "gray",
+  "Semilla" : "green",
+  "En Consolidación" : "orange",
+  "Referente" : "red"
+}
+
+function MarkerComp({ e, onSelect}){
+    return(
+        <Marker icon= {myMarker(consolidationStatus[e.sendero_ecobarrio])} position={[e.lat, e.lon]} key={e.id} eventHandlers={{click: () => onSelect({e})}}>
+                      <Popup>
+                        <div>
+                          <h3>{e.nombre}</h3>
+                        </div>
+                      </Popup>
+        </Marker>
+    )
+}
+
+export default MarkerComp;
