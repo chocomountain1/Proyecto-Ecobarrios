7f2e771b463ca48e6b83c5920c8b45647a5a1dbd
chocomountain1 <terminator32158@gmail.com>
2026-01-23 11:41:35 -0300

    Feature: add a way to visualize each solution

--- ecobarrios_ui/src/components/proyects.jsx
+++ ecobarrios_ui/src/components/proyects.jsx
@@ -1,6 +1,9 @@
 import '../styles/components/modal/modal.css';
+import React from 'react';
 
-export default function Proyect({ solutions }){
+export default function Proyect({ solutions}){
+    const [openId, setOpenId] = React.useState(null)
+
     console.log(solutions);
     if(!solutions || solutions.length === 0){
         return(
@@ -9,13 +12,20 @@ export default function Proyect({ solutions }){
             </div>
         )
     }
+
+    const handleToggle= (id) => {
+        setOpenId(openId == id ? null: id)
+    }
+
     return(
         <div className = "modal-title-section">
-            <div className = "modal-section-content">
+            <div className = "modal-proyect-content">
                 {solutions.map((solution, index) => (
                     <div key={index} className = "modal-proyect-card">
-                        <h3 className = "modal-h3-text">{solution.nombre_proyecto}</h3>
-                        <p className = "modal-p-text">{solution.propuesta_solucion}</p>
+                        <div className = "modal-proyect-title" onClick={() => handleToggle(solution.id)}>
+                        <h3 className = "modal-h3-text" style = {{ textDecoration: "underline"}}><strong>{solution.nombre_proyecto}</strong></h3>
+                        </div>
+                        {openId== solution.id &&(<p key = {index}className = "modal-p-text"><strong>Solución propuesta:</strong> {solution.propuesta_solucion}</p>)}
                     </div>
                 ))}
             </div>
