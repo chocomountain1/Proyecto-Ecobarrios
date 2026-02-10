1af4c7ba02c2cc2720e88446ba3dab60e65fe968
chocomountain1 <terminator32158@gmail.com>
2026-02-09 23:52:26 -0300

    test with new import

--- ecobarrios_ui/src/components/panel_botones.jsx
+++ ecobarrios_ui/src/components/panel_botones.jsx
@@ -1,4 +1,4 @@
-import Button from './Button';
+import Button from './Button.jsx';
 import '../styles/components/elements/panel_botones.css';
 
 export default function Panel_botones({array_textos, array_rutas}) {
