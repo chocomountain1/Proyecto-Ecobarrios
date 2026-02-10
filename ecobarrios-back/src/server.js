a80d149037d74d0590d925b4a6603a6341e6b64e
chocomountain1 <terminator32158@gmail.com>
2026-02-10 20:38:09 -0300

    change cors configuration for the communication of back and front

--- ecobarrios-back/src/server.js
+++ ecobarrios-back/src/server.js
@@ -8,7 +8,10 @@ import getDesafioByEcobarrio from "./controllers/desafioController.js";
 import { createProblema } from "./controllers/problemController.js";
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.get("/api/ecobarrios/sinFiltros", getAllEcobarrios); //get de ecobarrios
 app.post("/api/ecobarrios", getEcobarrios); // ruta api ecobarrios + filtros
